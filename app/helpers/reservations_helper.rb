f89c5ae0e3b5b1fb277b34df53911d1edfd77744
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-17 07:36:50 +0000

    [Add]予約の有無での画面が変更するように記述

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -23,4 +23,20 @@ module ReservationsHelper
             ]
   end
   
+  def check_reservation(reservations, day, time) #指定された日付と時間に対する予約の存在を確認
+    result = false
+    reservations_count = reservations.count #渡された予約データの要素数を代入
+    # 取得した予約データにdayとtimeが一致する場合はtrue,一致しない場合はfalseを返します
+    if reservations_count > 1
+      reservations.each do |reservation|
+        result = reservation[:day].eql?(day.strftime("%Y-%m-%d")) && reservation[:time].eql?(time) #予約の日付と時間が引数と渡された日付と時間が一致するか確認し、代入
+        return result if result
+      end
+    elsif reservations_count == 1
+      result = reservations[0][:day].eql?(day.strftime("%Y-%m-%d")) && reservations[0][:time].eql?(time)
+      return result if result
+    end
+    return result
+  end
+  
 end
