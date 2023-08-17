76dc5fc44f9fab382947028d5ad614af0c34ef79
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-17 07:12:52 +0000

    [Add]カレンダー表示

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,7 @@
 class ReservationsController < ApplicationController
+  
+  def index
+    @reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   
   resources :users
   resources :teachers
+  resources :reservations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
