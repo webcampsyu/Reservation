2e61b87f661a3b0e668b31503d939dd000f1756d
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-21 20:52:17 +0000

    [Add] UserとTeacherのアソシエーション

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,9 @@
 class Teacher < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  has_many :reservations, dependent: :destroy
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
