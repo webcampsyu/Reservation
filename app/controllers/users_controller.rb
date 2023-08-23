1152c9ab24e748d6533ce49daee0f5707df61738
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-23 20:57:13 +0000

    [Edit]マイページの予約を開始時間の昇順になるように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     #特定のユーザーに関する予約データを取得
-    @user_reservations = @user.reservations.where("start_time >= ?", DateTime.current).order(start_time: :desc) 
+    @user_reservations = @user.reservations.where("start_time >= ?", DateTime.current).order(:start_time) 
   end 
   
 end
