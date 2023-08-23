12dfd8f8d9bba03e679a2bac37fc3340c9ab925a
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-23 20:53:55 +0000

    [Add]Teacherのマイページを作成

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,5 +2,8 @@ class TeachersController < ApplicationController
   before_action :authenticate_teacher!
   
   def show
+    @teacher = current_teacher
+    #予約データの取得
+    @teacher_reservations = @teacher.reservations.where("user_id IS NOT NULL").where("start_time" >= ?", DateTime.current).order(start_time: :desc) # where("user_id IS NOT NULL")はuser_idカラムがNULLではないもの（つまりユーザーが予約した）データを選択
   end
 end
--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,2 +1,9 @@
 module TeachersHelper
+  
+  #引数として与えられた予約オブジェクトから関連するユーザーを検索し、ユーザー名を返す処理
+  def check_user(reservation)
+    user = User.find(reservation.user_id)
+    return user.user_name
+  end 
+  
 end
