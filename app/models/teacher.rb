67ad9a532e46116282abb4b09efd47e6b19b66c2
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-28 07:35:14 +0000

    [Add]TeacherモデルとUserモデルのバリデーションを作成

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,6 +6,9 @@ class Teacher < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :temp_reservations, dependent: :destroy
   
+  validates :email, :teacher_name, :teacher_area, :teacher_img, :teacher_address, presence: true
+  validates :email, uniqueness: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :temp_reservations, dependent: :destroy
   
+  validates :email, :user_name, :user_area, :curriculum, :curriculum_num, :video_availabel, :user_address, presence: true
+  validates :email, uniqueness: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
