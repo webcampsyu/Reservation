55cfdc3d4fbb40dfc484e1c505e1b233ab5f0b9d
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-28 02:22:04 +0000

    [Edit]コードの簡略化

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -3,13 +3,13 @@ module TeachersHelper
   #引数として与えられた予約オブジェクトから関連するユーザーを検索し、ユーザー名を返す処理
   def check_user(reservation)
     user = User.find(reservation.user_id)
-    return user.user_name
+    user.user_name
   end 
   
   #特定の予約オブジェクトを引数として渡し、その予約に対応するUserの住所情報を返す処理
   def user_address(reservation)
     user = User.find(reservation.user_id)
-    return user.user_address
+    user.user_address
   end 
   
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,12 +2,12 @@ module UsersHelper
   
   def check_teacher(reservation)
     teacher = Teacher.find(reservation.teacher_id)
-    return teacher.teacher_name
+    teacher.teacher_name
   end 
   
   def teacher_address(reservation)
     teacher = Teacher.find(reservation.teacher_id)
-    return teacher.teacher_address
+    teacher.teacher_address
   end 
   
   
