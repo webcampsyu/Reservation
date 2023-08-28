24feb7bdf25dcfb499e2b5a0093fb5b8418c40b9
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-28 04:31:13 +0000

    [Edit]Teacherマイページで仮予約を確認できるように設定

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -5,5 +5,6 @@ class TeachersController < ApplicationController
     @teacher = current_teacher
     #予約データの取得
     @teacher_reservations = @teacher.reservations.where("user_id IS NOT NULL").where("start_time" >= ?", DateTime.current).order(:start_time) # where("user_id IS NOT NULL")はuser_idカラムがNULLではないもの（つまりユーザーが予約した）データを選択
+    @temp_reservations = @teacher.temp_reservations.where("user_id IS NOT NULL").where("start_time >= ?", DateTime.current).order(:start_time)
   end
 end
