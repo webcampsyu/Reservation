afa415260e8a75dc5a42f00c3937e5702714a78d
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-29 04:03:03 +0000

    [Edit]action_mailerの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  
+  config.action_mailer.default_url_options = { host: 'jsmc-machido.com' }
+  config.aciton_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address => ENV['MAIL_SERVER'],
+    :port => 587,
+    :user_name => ENV['MAIL_USERNAME'],
+    :password => ENV['MAIL_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
