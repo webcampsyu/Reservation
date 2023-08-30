6a339aabf80bbcf651cc8872a6655b303eab7517
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-08-30 20:42:44 +0000

    [Edit]文言修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,26 @@
 Rails.application.routes.draw do
   
-  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get '/', to: 'user#show'
+  get '/', to: 'users#show'
   devise_for :teachers, controllers: {
-    sessions: 'teachers/sessions',
+    sessions:      'teachers/sessions',
     registrations: 'teachers/registrations'
   }
-  
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
+    sessions:      'users/sessions',
     registrations: 'users/registrations'
   }
-  
-  resources :users do 
-    resources :teachers do 
+  resources :users do
+    resources :teachers do
       resources :reservations
-    end 
+    end
     get '/choice', to: 'reservations#choice'
-  end 
+  end
   resources :teachers do
     get '/reservations/index', to: 'reservations#teacher_index'
     post '/reservations/index', to: 'reservations#teacher_create'
-    post '/reservatins/new', to: 'reservations#teacher_new'
+    post '/reservations/new', to: 'reservations#teacher_new'
     get '/reservations/all_day_new', to: 'reservations#all_day_new'
     get '/reservations/:id', to: 'reservations#teacher_show'
     delete '/temp_reservation/:id', to: 'temp_reservations#teacher_destroy', as: :destroy_temp_reservation
@@ -34,6 +31,7 @@ Rails.application.routes.draw do
       resources :temp_reservations
     end
   end
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
