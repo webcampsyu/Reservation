37f8167f20ed63e5481f470cd3cd85f8f387b70e
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-05 20:54:35 +0000

    [Edit]アプリケーション名変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Reservation_production"
+  # config.active_job.queue_name_prefix = "ReservationApp_production"
 
   config.action_mailer.perform_caching = false
 
