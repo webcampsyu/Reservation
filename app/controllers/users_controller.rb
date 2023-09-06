2f40be403943c5e935903219506d41cf0a892f69
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-06 20:42:25 +0000

    [Edit]マイページのエラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = current_user
     #特定のユーザーに関する予約データを取得
     @user_reservations = @user.reservations.where("start_time >= ?", DateTime.current).order(:start_time) 
-    @temp_reservations = @user.temp_reservation.where("start_time >= ?", DateTime.current).order(:start_time)
+    @temp_reservations = @user.temp_reservations.where("start_time >= ?", DateTime.current).order(:start_time)
   end 
   
 end
