bb8389ca39fa6a5ef7218dc6c26cb591b631b0ac
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-10 20:37:13 +0000

    [Edit]Teacherのparametersanitizer.rbファイルを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   
   protected
-  
-  def devise_parameter_sanitizer #新しいオプジェクトを作成または更新する際のパラメータ制御
-    if resource_class == User then 
-      Users::ParameterSanitizer.new(User, :user, params) #クラスの新しいインスタンスを作成。:userはキーで、これに対応する許可する属性が定義されたハッシュを参照
+  def devise_parameter_sanitizer # 新しいオプジェクトを作成または更新する際のパラメータ制御
+    if resource_class == User then
+      Users::ParameterSanitizer.new(User, :user, params) # クラスの新しいインスタンスを作成。:userはキーで、これに対応する許可する属性が定義されたハッシュを参照
     elsif resource_class == Teacher
       Teachers::ParameterSanitizer.new(Teacher, :teacher, params)
     else
