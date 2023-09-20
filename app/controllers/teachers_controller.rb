6553735163cedf1ada08f29ca3baa1d72024e2b3
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-20 20:53:42 +0000

    [Edit]エラー文修正

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -4,7 +4,7 @@ class TeachersController < ApplicationController
   def show
     @teacher = current_teacher
     #予約データの取得
-    @teacher_reservations = @teacher.reservations.where("user_id IS NOT NULL").where("start_time" >= ?", DateTime.current).order(:start_time) # where("user_id IS NOT NULL")はuser_idカラムがNULLではないもの（つまりユーザーが予約した）データを選択
+    @teacher_reservations = @teacher.reservations.where("user_id IS NOT NULL").where("start_time >= ?", DateTime.current).order(:start_time) # where("user_id IS NOT NULL")はuser_idカラムがNULLではないもの（つまりユーザーが予約した）データを選択
     @temp_reservations = @teacher.temp_reservations.where("user_id IS NOT NULL").where("start_time >= ?", DateTime.current).order(:start_time)
   end
 end
